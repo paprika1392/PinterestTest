70254bc004695eb428890304f1629de143bfb820
Sasha <aleksandrpaprika@gmail.com>
2024-10-10 13:32:25 +0400

    Have been created Constants class with constants url, login and password

---
+++ src/test/java/Utils/Constants.java
@@ -0,0 +1,7 @@
+package Utils;
+
+public class Constants {
+    public static final String PINTEREST_URL = "https://www.pinterest.com/";
+    public static final String LOGIN = "testAkkForPractiec@gmail.com";
+    public static final String PASSWORD = "paprika13921392";
+}
