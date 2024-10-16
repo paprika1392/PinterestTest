5e4406872e35d0efea59b23dcba9c8b96e6995ae
Sasha <aleksandrpaprika@gmail.com>
2024-10-16 14:59:15 +0400

    Have been added getAccountName method

--- src/test/java/PinterestPages/AccountPage.java
+++ src/test/java/PinterestPages/AccountPage.java
@@ -7,7 +7,7 @@ public class AccountPage extends BasePage {
     private final By currentAccountName = By.xpath("//div[@data-test-id='profile-name']");
 
 
-    public void getCurrentAccountName() {
+    public void getAccountName() {
         String accountName = driver.findElement(currentAccountName).getText();
     }
 
