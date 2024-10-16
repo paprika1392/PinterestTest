96770fe3a02d52011f4d7597a5d9a17e37069b63
Sasha <aleksandrpaprika@gmail.com>
2024-10-16 14:58:39 +0400

    Have been added homePageHeader field

--- src/test/java/PinterestPages/HomePage.java
+++ src/test/java/PinterestPages/HomePage.java
@@ -1,11 +1,14 @@
 package PinterestPages;
 
+import PageElements.HomePageHeader;
 import Utils.BrowserClass;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class HomePage extends BasePage{
+
+    public HomePageHeader homePageHeader = new HomePageHeader();
 
     private final By rootOfHomePage = By.xpath("//div[@id='__PWS_ROOT__']");
 
