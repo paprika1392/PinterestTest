5d652a15683d3b8c0251ed93062d8f730b3ac6c8
Sasha <aleksandrpaprika@gmail.com>
2024-11-19 15:16:53 +0400

    Changes of rename BrowserClass to Browser and added waiter

--- src/test/java/PageElements/HomePageHeader.java
+++ src/test/java/PageElements/HomePageHeader.java
@@ -1,12 +1,17 @@
 package PageElements;
 
 import PinterestPages.BasePage;
-import Utils.BrowserClass;
+import Utils.Browser;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class HomePageHeader extends BasePage {
 
     private final By headerProfile = By.xpath("//div[@data-test-id='header-profile']");
+
+    public void waitUntilHeaderProfileWillBeClickable() {
+        Browser.getWebdriverWait().until(ExpectedConditions.elementToBeClickable(headerProfile));
+    }
 
     public void clickHeaderProfile() {
         driver.findElement(headerProfile).click();
--- src/test/java/PinterestPages/HomePage.java
+++ src/test/java/PinterestPages/HomePage.java
@@ -1,9 +1,8 @@
 package PinterestPages;
 
 import PageElements.HomePageHeader;
-import Utils.BrowserClass;
+import Utils.Browser;
 import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class HomePage extends BasePage{
@@ -13,7 +12,7 @@ public class HomePage extends BasePage{
     private final By rootOfHomePage = By.xpath("//div[@id='__PWS_ROOT__']");
 
     public void waitHomePageIsLoaded() {
-        BrowserClass.getWebdriverWait().until(ExpectedConditions.visibilityOfElementLocated(rootOfHomePage));
+        Browser.getWebdriverWait().until(ExpectedConditions.visibilityOfElementLocated(rootOfHomePage));
     }
 
 }
