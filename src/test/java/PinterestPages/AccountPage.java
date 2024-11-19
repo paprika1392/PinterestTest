16324222fb46aa2fc406ddf943e055eb4e20c845
Sasha <aleksandrpaprika@gmail.com>
2024-11-19 15:06:31 +0400

    Have been added waiter for currentAccountName

--- src/test/java/PinterestPages/AccountPage.java
+++ src/test/java/PinterestPages/AccountPage.java
@@ -1,14 +1,21 @@
 package PinterestPages;
 
+import Utils.Browser;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class AccountPage extends BasePage {
 
+    public static String accountName;
+
     private final By currentAccountName = By.xpath("//div[@data-test-id='profile-name']");
 
+    public  void waitUntilCurrentAccountNameWillBeVisible() {
+        Browser.getWebdriverWait().until(ExpectedConditions.visibilityOfElementLocated(currentAccountName));
+    }
 
     public void getAccountName() {
-        String accountName = driver.findElement(currentAccountName).getText();
+        accountName = driver.findElement(currentAccountName).getText();
     }
 
 
