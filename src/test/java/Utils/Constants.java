371d62b6dffd51a71c407659c1a8994bb89adfff
Sasha <aleksandrpaprika@gmail.com>
2024-11-19 15:11:52 +0400

    Have been added ACTUAL_ACCOUNT_NAME

--- src/test/java/Utils/Constants.java
+++ src/test/java/Utils/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
     public static final String PINTEREST_URL = "https://www.pinterest.com/";
     public static final String LOGIN = "testAkkForPractiec@gmail.com";
     public static final String PASSWORD = "paprika13921392";
+    public static final String ACTUAL_ACCOUNT_NAME = "Testakkforpractiec";
 }
