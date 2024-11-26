89603a9c7857703d7bf6263db03bb27995e8c42d
Sasha <aleksandrpaprika@gmail.com>
2024-11-26 14:37:21 +0400

    have been deleted locator

--- src/test/java/PinterestPages/AccountPage.java
+++ src/test/java/PinterestPages/AccountPage.java
@@ -10,8 +10,6 @@ public class AccountPage extends BasePage {
 
     private final By currentAccountName = By.xpath("//div[@data-test-id='profile-name']");
 
-    private final By editButton = By.xpath("//button[@aria-label='Edit']");
-
     public  void waitUntilCurrentAccountNameWillBeVisible() {
         Browser.getWebdriverWait().until(ExpectedConditions.visibilityOfElementLocated(currentAccountName));
     }
