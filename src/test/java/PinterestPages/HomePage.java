190e0087051584b1858d4ab0c57f146357962c0a
Sasha <aleksandrpaprika@gmail.com>
2024-11-26 13:55:48 +0400

    have been deleted same methods and locators

--- src/test/java/PinterestPages/HomePage.java
+++ src/test/java/PinterestPages/HomePage.java
@@ -1,5 +1,6 @@
 package PinterestPages;
 
+import PageElements.FirstDataGridElement;
 import PageElements.HomePageHeader;
 import Utils.Browser;
 import org.openqa.selenium.By;
@@ -8,22 +9,12 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 public class HomePage extends BasePage{
 
     public HomePageHeader homePageHeader = new HomePageHeader();
+    public FirstDataGridElement firstDataGridElement = new FirstDataGridElement();
 
     private final By rootOfHomePage = By.xpath("//div[@id='__PWS_ROOT__']");
-    private final By firstDataGridItem = By.xpath("//div[@data-grid-item-idx='0']");
-    private final By saveButton = By.xpath("//button[@aria-label='Save']");
 
     public void waitHomePageIsLoaded() {
         Browser.getWebdriverWait().until(ExpectedConditions.visibilityOfElementLocated(rootOfHomePage));
-    }
-
-    public void selectFirstDataGridItem() {
-        driver.findElement(firstDataGridItem).isSelected();
-    }
-
-    public void clickSaveButton() {
-        Browser.getWebdriverWait().until(ExpectedConditions.visibilityOfElementLocated(saveButton));
-        driver.findElement(saveButton).click();
     }
 
 }
