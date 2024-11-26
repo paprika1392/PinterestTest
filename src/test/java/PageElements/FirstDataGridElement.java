dd6f57599a9558983b436a04df022d30f040b9fd
Sasha <aleksandrpaprika@gmail.com>
2024-11-26 14:37:50 +0400

    have been created locators and methods

--- src/test/java/PageElements/FirstDataGridElement.java
+++ src/test/java/PageElements/FirstDataGridElement.java
@@ -10,6 +10,8 @@ public class FirstDataGridElement extends BasePage {
     private final By firstDataGridItem = By.xpath("//div[@data-grid-item-idx='0']");
     private final By saveButton = By.xpath("//button[@aria-label='Save']");
     private final By deleteButton = By.xpath("//div[@data-test-id='delete-pin-button']");
+    private final By editButton = By.xpath("//button[@aria-label='Edit']");
+    private final By confirmDeleteButton = By.xpath("//div[@data-test-id='confirm-delete-pin']");
 
     public void selectFirstDataGridItem() {
         driver.findElement(firstDataGridItem).isSelected();
@@ -24,8 +26,19 @@ public class FirstDataGridElement extends BasePage {
         driver.findElement(firstDataGridItem).click();
     }
 
+    public void clickEditButton() {
+        Browser.getWebdriverWait().until(ExpectedConditions.visibilityOfElementLocated(editButton));
+        driver.findElement(editButton).click();
+    }
+
     public void clickDeleteButton() {
         Browser.getWebdriverWait().until(ExpectedConditions.visibilityOfElementLocated(deleteButton));
         driver.findElement(deleteButton).click();
     }
+
+    public void clickConfirmDeleteButton() {
+        Browser.getWebdriverWait().until(ExpectedConditions.visibilityOfElementLocated(confirmDeleteButton));
+        driver.findElement(confirmDeleteButton).click();
+    }
+
 }
