0200a8d80fa3958dde86dfb3bde5cfa7f47d54c3
Sasha <aleksandrpaprika@gmail.com>
2025-01-27 11:50:49 +0400

    have been added new methods

--- src/test/java/Tests/AddAndDeleteElementTest.java
+++ src/test/java/Tests/AddAndDeleteElementTest.java
@@ -1,6 +1,8 @@
 package Tests;
 
+import PageElements.FirstDataGridElement;
 import Steps.Steps;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static Utils.Constants.LOGIN;
@@ -10,9 +12,12 @@ public class AddAndDeleteElementTest extends BaseTest {
     @Test
     public void addAndDeleteElementTest() {
         Steps.enterLoginAndPassword(LOGIN, PASSWORD);
-        Steps.selectAndSaveFirstDataGridElement();
-        Steps.goToPersonalProfile();
-        Steps.selectAndDeleteFirstDataGridElement();
+        Steps.addFirstDataGridElement();
+        Steps.goToAccountPage();
+        Steps.reloadCurrentPage();
+        Assert.assertTrue(FirstDataGridElement.displayOfFirstDataGridItem, "The element is not displayed");
+        Steps.deleteFirstDataGridElement();
+        Assert.assertFalse(FirstDataGridElement.displayOfFirstDataGridItem, "The element is till displayed");
       }
 
 }
